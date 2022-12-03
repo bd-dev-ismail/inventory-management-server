62eed03561be1c0ebc46d10c53a369634068d138
bd-dev-ismail <ismailratul7@gmail.com>
2022-12-03 16:54:32 +0600

    user saved

--- index.js
+++ index.js
@@ -19,6 +19,40 @@ const client = new MongoClient(uri, {
 
 async function run(){
     const usersCollection = client.db('inventoryMangement').collection('users');
+    //save register user
+    app.post('/register', async(req, res)=> {
+        const user = req.body;
+        // const query = {email: user.email}
+        // const oldUser = await usersCollection.findOne(query);
+        // if(oldUser.email === user.email){
+        //     return res.send({message: "Email Already Exist! Please Enter A New Email!", register: false});
+        // }
+        // else{
+        //      const result = await usersCollection.insertOne(user);
+        //      res.send(result);
+        // }
+         const result = await usersCollection.insertOne(user);
+         res.send(result);
+       
+    });
+    //login user
+    app.post('/login', async(req, res)=> {
+        const user = req.body;  
+        const queryEmail = {email: user.email}
+        const oldUser = await usersCollection.findOne(queryEmail);
+        //if user not found
+        if(!oldUser){
+            return res.send({email: false})
+        };
+        //if password not matched
+        if(oldUser.password !== user.password){
+            return res.send({password: false})
+        };
+        //if email & pass matched
+        if((user.email === oldUser.email) && (user.password === oldUser.password)){
+            res.send({login: true});
+        }
+    })
 }
 run().catch(error=> console.log(error))
 
